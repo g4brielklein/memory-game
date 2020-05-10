91a67941a2303b5cb7ae3df3b0d9fcdb82e92ef1
g4brielklein <gabrielkleinribas@gmail.com>
2020-05-10 03:31:55 -0300

    Shuffling cards

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -13,9 +16,8 @@ function flipCard() {
 
         return;
     }
-    
+
     // second click
-    hasFlippedCard = false;
     secondCard = this;
 
     checkForMatch();
@@ -30,14 +32,32 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
